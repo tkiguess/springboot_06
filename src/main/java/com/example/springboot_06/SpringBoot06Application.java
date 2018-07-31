a61d36414aec315ccd77ed0fdc25190c88389b93
tkiguess <ahmedtafkir@gmail.com>
2018-07-31 16:46:28 -0400

    Initial commit

---
+++ src/main/java/com/example/springboot_06/SpringBoot06Application.java
@@ -0,0 +1,12 @@
+package com.example.springboot_06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot06Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot06Application.class, args);
+	}
+}
